542ebf189679e369d0ace534d9aa161522ef134f
jxadica <kjanalyeffa@gmail.com>
2024-06-09 12:34:39 +0400

    last

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let menu= document.querySelector(".menuButton");
+let close= document.querySelector(".close");
+let phone= document.querySelector(".phone");
+
+
 document.addEventListener("DOMContentLoaded", function() {
     let logo = document.querySelector(".logo");
 
@@ -7,5 +12,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     logo.addEventListener("mouseout", function() {
         logo.src = 'images/logo.png';
+    });
+});
+
+menu.addEventListener("click", function(){
+    menu.style.display="none";
+    close.style.display="inline-block";
+    phone.style.display="flex";
+})
+close.addEventListener("click", function(){
+    menu.style.display="inline-block";
+    close.style.display="none";
+    phone.style.display="none";
+
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+            menuItems.forEach(i => i.classList.remove('active'));
+            item.classList.add('active');
+        });
     });
 });
